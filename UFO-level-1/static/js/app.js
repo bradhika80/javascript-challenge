dd70d693633f7a958ce784680e46bda2fd7b28d9
bradhika80 <bradhika80@gmail.com>
2020-11-11 20:20:35 -0600

    add handler for the button

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,16 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// get the button handler
+
+d3.select("#filter-btn").on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputValue = d3.select("#datetime").node().value;
+
+
+})
