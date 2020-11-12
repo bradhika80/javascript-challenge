26209a41f309bc212b727f191da293fc0e5899a7
bradhika80 <bradhika80@gmail.com>
2020-11-11 23:28:40 -0600

    add date validations

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -10,8 +10,52 @@ d3.select("#filter-btn").on("click", function() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
 
-  // Select the input element and get the raw HTML node
+  // Select the input element and get the value
   var inputValue = d3.select("#datetime").node().value;
+
+  console.log(inputValue)
+
+  validationMsg = ValidateDate(inputValue);
+
+  // if date validation is not successful then print the error message and exit the event handler
+  if (validationMsg != "success")
+  {
+     d3.select("#DateInvalid").text(validationMsg)
+     return;
+
+  }
+  else
+  {
+    d3.select("#DateInvalid").text("")
+  }
+
+  // filter the data based on the input
+
+
 
 
 })
+
+
+// a function to validate the date input
+function ValidateDate(dateStr)
+{
+    // checks if the date input is empty
+    if (dateStr === null || dateStr.trim() === '')
+    {       
+        return "input value empty!!";  
+    }
+
+    // regex expression for the date format
+    //dateFormat = /(0\d{1}|1[0-2])\/([0-2]\d{1}|3[0-1])\/(19|20)\d{2}/
+    dateFormat = /(0\d{1}|1[0-2])\/([0-2]\d{1}|3[0-1])\/\d{4}/
+
+    // if the input does not match the date format, it returns false
+    if (!dateStr.trim().match(dateFormat))
+    {    
+        return "date format invalid!!" ;
+    }
+
+    return "success";
+
+}
